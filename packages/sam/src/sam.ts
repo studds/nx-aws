a0a36a5592421aff6e17e439d4df752f68a72bfc
studds <daniel.studds@gmail.com>
2019-07-08 07:48:19 +1000

    Modify builder to get entries from cf template

--- packages/sam/src/sam.ts
+++ packages/sam/src/sam.ts
@@ -3,7 +3,7 @@ import { JsonObject, workspaces } from '@angular-devkit/core';
 import { runWebpack, BuildResult } from '@angular-devkit/build-webpack';
 
 import { Observable, from } from 'rxjs';
-import { writeFileSync } from 'fs';
+import { writeFileSync, readFileSync } from 'fs';
 import { resolve, parse } from 'path';
 import { map, concatMap } from 'rxjs/operators';
 import { getNodeWebpackConfig } from '@nrwl/node/src/utils/node.config';
@@ -12,9 +12,14 @@ import { BuildBuilderOptions } from '@nrwl/node/src/utils/types';
 import { normalizeBuildOptions } from '@nrwl/node/src/utils/normalize';
 import { NodeJsSyncHost } from '@angular-devkit/core/node';
 import webpackMerge from 'webpack-merge';
-import { Configuration } from 'webpack';
-import { sync as glob } from 'globby';
+import { Configuration, Entry } from 'webpack';
 import { camelize } from '@angular-devkit/core/src/utils/strings';
+
+import { load, dump } from 'js-yaml';
+import { CLOUDFORMATION_SCHEMA } from 'cloudformation-js-yaml-schema';
+import Resource from 'cloudform-types/types/resource';
+import Template from 'cloudform-types/types/template';
+import { sync as mkdirp } from 'mkdirp';
 
 try {
     require('dotenv').config();
@@ -43,19 +48,8 @@ function run(
         ),
         map(options => {
             const defaultConfig: Configuration = getNodeWebpackConfig(options);
-            let config = webpackMerge(defaultConfig, getMore());
-            console.log(options, context);
-
-            const handlers = glob(options.handlerPattern, {
-                cwd: options.root
-            });
-            const entries: { [name: string]: string[] } = {};
-            handlers.map(handler => {
-                const name = camelize(parse(handler).name);
-                entries[name] = [/*'./src/source-map-install.ts',*/ handler];
-            });
-            config.entry = entries;
-            console.log(config);
+            let config = webpackMerge(defaultConfig, getCustomWebpack());
+            config.entry = getEntriesFromCloudformation(options, context);
             if (options.webpackConfig) {
                 config = require(options.webpackConfig)(config, {
                     options,
@@ -114,17 +108,88 @@ async function getSourceRoot(context: BuilderContext) {
     }
 }
 
-function getMore(): Configuration {
+/**
+ * Read the cloudformation template yaml, and use it to identify our input files.
+ */
+function getEntriesFromCloudformation(
+    options: JsonObject & BuildNodeBuilderOptions,
+    context: BuilderContext
+): Entry {
+    // todo: Question: Is using sync file i/o OK in these builders? Did this initially
+    // to get things working, then noticed that the node builder also does some sync io?
+    const yaml = readFileSync(options.main, { encoding: 'utf-8' });
+    const cf: Template = load(yaml, { schema: CLOUDFORMATION_SCHEMA });
+
+    const resources = cf.Resources;
+
+    if (!resources) {
+        throw new Error("Cloudformation template didn't contain any resources");
+    }
+
+    const functions: Resource[] = [];
+
+    Object.keys(resources).forEach(name => {
+        if (resources[name].Type === 'AWS::Serverless::Function') {
+            functions.push(resources[name]);
+        }
+    });
+
+    if (functions.length === 0) {
+        throw new Error(
+            "Cloudformation template didn't contain any AWS::Serverless::Function's"
+        );
+    }
+
+    const { dir, base } = parse(options.main);
+
+    const handlers = functions
+        .filter(
+            (
+                fn
+            ): fn is Resource & {
+                Properties: { [key: string]: any };
+            } => !!fn.Properties
+        )
+        .map(fn => {
+            const codeUri = fn.Properties.CodeUri;
+            const src = resolve(dir, codeUri);
+            const name = camelize(parse(src).name);
+            // NB: this must match the output filename schema in the webpack config below
+            const newCodeUri = `${name}/${name}.js`;
+            fn.Properties.CodeUri = newCodeUri;
+            return { src, name };
+        });
+
+    const alteredYaml = dump(cf, { schema: CLOUDFORMATION_SCHEMA });
+
+    // todo: Question: Is creating this directory here really in keeping with the spirit of the thing?
+    mkdirp(resolve(context.workspaceRoot, options.outputPath));
+    writeFileSync(
+        resolve(context.workspaceRoot, options.outputPath, base),
+        alteredYaml,
+        {
+            encoding: 'utf-8'
+        }
+    );
+
+    const entries: { [name: string]: string[] } = {};
+    handlers.map(handler => {
+        const { name, src } = handler;
+        entries[name] = [/*'./src/source-map-install.ts',*/ src];
+    });
+    return entries;
+}
+
+function getCustomWebpack(): Configuration {
     return {
-        // todo: need to generate these
         output: {
             libraryTarget: 'commonjs',
+            // we create each chunk in it's own directory: this makes it easy to upload independent packages
             filename: '[name]/[name].js'
         },
         externals: {
-            'aws-sdk': 'aws-sdk',
-            'chrome-aws-lambda': 'chrome-aws-lambda',
-            'puppeteer-core': 'puppeteer-core'
+            // use the aws-sdk provided on the lambda instead of uploading it
+            'aws-sdk': 'aws-sdk'
         },
         optimization: {
             minimize: false
