3d5ef9c0a8a53fd743088bc0f30f60f88173d1ca
studds <daniel.studds@gmail.com>
2019-08-03 19:45:57 +1000

    Package: Adding support for AWS::Serverless::Application

--- packages/sam/src/cloudformation/package/package.ts
+++ packages/sam/src/cloudformation/package/package.ts
@@ -1,7 +1,16 @@
-import { createBuilder } from '@angular-devkit/architect';
+import { createBuilder, BuilderContext } from '@angular-devkit/architect';
 import { JsonObject } from '@angular-devkit/core';
 import { runCloudformationCommand } from '../run-cloudformation-command';
-
+import { sync as mkdirpSync } from 'mkdirp';
+import { parse } from 'path';
+import { loadCloudFormationTemplate } from '../../utils/load-cloud-formation-template';
+import Template from 'cloudform-types/types/template';
+import { from } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { resolve } from 'path';
+import { writeFileSync } from 'fs';
+import { dump } from 'js-yaml';
+import Resource from 'cloudform-types/types/resource';
 // todo: allow overriding some / all of these with environment variables
 interface IPackageOptions extends JsonObject {
     /**
@@ -28,6 +37,69 @@ try {
     require('dotenv').config();
 } catch (e) {}
 
-export default createBuilder<IPackageOptions>((options, context) => {
-    return runCloudformationCommand(options, context, 'package');
-});
+export default createBuilder<IPackageOptions>(
+    (options: IPackageOptions, context: BuilderContext) => {
+        const cloudFormation = loadCloudFormationTemplate(options.templateFile);
+        return from(updateCloudFormationTemplate(cloudFormation, context)).pipe(
+            switchMap(() => {
+                const outputPath = parse(options.outputTemplateFile).dir;
+                mkdirpSync(outputPath);
+                const updatedTemplateFile = resolve(
+                    outputPath,
+                    parse(options.templateFile).base
+                );
+                options.templateFile = updatedTemplateFile;
+                writeFileSync(updatedTemplateFile, dump(cloudFormation), {
+                    encoding: 'utf-8'
+                });
+                // todo: probably should use nrwl's command builder (whatever that's called?)
+                return runCloudformationCommand(options, context, 'package');
+            })
+        );
+    }
+);
+async function updateCloudFormationTemplate(
+    cloudFormation: Template,
+    context: BuilderContext
+) {
+    const resources = cloudFormation.Resources;
+    if (resources) {
+        for (const key in resources) {
+            if (resources.hasOwnProperty(key)) {
+                const resource = resources[key];
+                if (resource.Type === 'AWS::Serverless::Application') {
+                    await resolveSubStackTemplateLocation(
+                        resource,
+                        context,
+                        key
+                    );
+                }
+            }
+        }
+    }
+}
+async function resolveSubStackTemplateLocation(
+    resource: Resource,
+    context: BuilderContext,
+    key: string
+) {
+    const properties = resource.Properties;
+    if (properties) {
+        const location = properties.Location;
+        const applicationOptions = await context.getTargetOptions({
+            project: location,
+            target: 'package'
+        });
+        const outputTemplateFile = applicationOptions.outputTemplateFile;
+        if (typeof outputTemplateFile === 'string' && outputTemplateFile) {
+            const finalTemplateLocation = resolve(
+                context.workspaceRoot,
+                outputTemplateFile
+            );
+            context.logger.info(
+                `Remapping ${key} location to ${finalTemplateLocation} for referenced project ${location}`
+            );
+            properties.Location = finalTemplateLocation;
+        }
+    }
+}
