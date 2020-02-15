bf0610a602a6a3674ef9ff0b18d7c8c1d3964578
Daniel Studds <studds@fastmail.com>
2020-02-15 11:19:44 +1100

    Shifting across to using nx

--- libs/sam/src/lib/sam.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sam } from './sam';
-
-describe('sam', () => {
-  it('should work', () => {
-    expect(sam()).toEqual('sam');
-  });
-});
--- libs/sam/src/lib/sam.ts
+++
@@ -1,3 +0,0 @@
-export function sam(): string {
-  return 'sam';
-}
