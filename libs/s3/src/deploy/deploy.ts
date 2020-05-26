07734aef9180bdb791d465f0da904a27576252ff
Daniel Studds <studds@fastmail.com>
2020-05-26 13:07:44 +1000

    s3:deploy: create & upload an env specific config

--- libs/s3/src/deploy/deploy.ts
+++ libs/s3/src/deploy/deploy.ts
@@ -7,10 +7,13 @@ import {
     ImportStackOutput,
     ImportStackOutputs
 } from '@nx-aws/core';
+import { writeFileSync } from 'fs';
+import { resolve } from 'path';
 
 export interface S3DeployOptionsResource extends JsonObject {
     include: string[];
     cacheControl: string;
+    invalidate: boolean;
 }
 
 export interface S3DeployOptions extends JsonObject {
@@ -19,6 +22,10 @@ export interface S3DeployOptions extends JsonObject {
     distribution: (ImportStackOutput & JsonObject) | null;
     destPrefix: string | null;
     stackSuffix: string | null;
+    config: {
+        importStackOutputs: ImportStackOutputs & JsonObject;
+        configFileName: string;
+    } | null;
 }
 
 const outputValueRetriever = new OutputValueRetriever();
@@ -28,39 +35,52 @@ try {
 } catch (err) {}
 
 export default createBuilder<S3DeployOptions>((options, context) => {
-    const resources = normaliseResources(options.resources);
+    const config = options.config;
+    const resources = normaliseResources(
+        options.resources,
+        config?.configFileName
+    );
 
     return from(getOutputDir(context)).pipe(
-        switchMap(async outputDir => {
-            const map: ImportStackOutputs = { bucket: options.bucket };
+        switchMap(async (outputDir) => {
+            const map = config?.importStackOutputs || {};
+            map.bucket = options.bucket;
             if (options.distribution) {
                 map.distribution = options.distribution;
             }
             const stackSuffix = options.stackSuffix || undefined;
             const {
                 bucket,
-                distribution
+                distribution,
+                // any remaining output values will be those specified in config.importStackOutputs
+                ...configFile
             } = await outputValueRetriever.getOutputValues(
                 map,
                 context,
                 stackSuffix
             );
+            if (config) {
+                // write any values imported for config to the output directory
+                writeFileSync(
+                    resolve(outputDir, config.configFileName),
+                    JSON.stringify(configFile),
+                    { encoding: 'utf-8' }
+                );
+            }
             const destPrefix = options.destPrefix || '';
             const commands = resourceToS3Command(
                 resources,
                 outputDir,
                 bucket,
-                destPrefix
+                destPrefix,
+                distribution
             );
-            commands.push({
-                command: `aws cloudfront create-invalidation --distribution-id ${distribution} --paths /index.html`
-            });
             return context.scheduleBuilder('@nrwl/workspace:run-commands', {
                 parallel: false,
                 commands
             });
         }),
-        switchMap(run => run.output)
+        switchMap((run) => run.output)
     );
 });
 
@@ -81,20 +101,32 @@ async function getOutputDir(context: BuilderContext) {
 }
 
 function normaliseResources(
-    resources: S3DeployOptionsResource[] | null
+    resources: S3DeployOptionsResource[] | null,
+    configFileName: string | undefined
 ): S3DeployOptionsResource[] {
     if (resources && resources.length > 0) {
         return resources;
     }
+    const dynamicResources = ['index.html'];
+    if (configFileName) {
+        dynamicResources.push(configFileName);
+    }
     return [
-        { include: ['index.html'], cacheControl: 'public, max-age=300' },
+        {
+            include: dynamicResources,
+            cacheControl: 'public, max-age=1, stale-while-revalidate=300',
+            invalidate: true
+        },
         {
             include: ['*.js', '*.css', '*.woff'],
-            cacheControl: 'public, max-age=315360000, immutable'
+            cacheControl: 'public, max-age=315360000, immutable',
+            invalidate: false
         },
         {
             include: [],
-            cacheControl: 'public, max-age=86400'
+            cacheControl:
+                'public, max-age=86400, stale-while-revalidate=315360000',
+            invalidate: false
         }
     ];
 }
@@ -103,22 +135,47 @@ function resourceToS3Command(
     resources: S3DeployOptionsResource[],
     outputDir: string,
     bucketName: string,
-    destPrefix: string
+    destPrefix: string,
+    distribution: string
 ) {
     const allIncludes = resources.reduce<string[]>((acc, resource) => {
         return [...acc, ...resource.include];
     }, []);
     const s3Path = destPrefix ? `${bucketName}/${destPrefix}` : `${bucketName}`;
     const commands = resources.map(({ include, cacheControl }) => {
-        const includePhrases = include.map(i => `--include "${i}"`).join(' ');
+        const includePhrases = include.map((i) => `--include "${i}"`).join(' ');
         const excludePhrases =
             include.length > 0
                 ? `--exclude "*"`
                 : allIncludes
-                      .filter(i => include.includes(i))
-                      .map(i => `--exclude "${i}"`);
+                      .filter((i) => include.includes(i))
+                      .map((i) => `--exclude "${i}"`);
         const command = `aws s3 sync --delete ${excludePhrases} ${includePhrases} ${outputDir} s3://${s3Path} --cache-control "${cacheControl}"`;
         return { command };
     });
+    appendInvalidationCommand(distribution, resources, commands);
     return commands;
+}
+
+function appendInvalidationCommand(
+    distribution: string,
+    resources: S3DeployOptionsResource[],
+    commands: { command: string }[]
+) {
+    if (distribution) {
+        const resourcesToInvalidate = resources.filter(
+            (needle) => needle.invalidate
+        );
+        if (resourcesToInvalidate.length > 1) {
+            throw new Error(
+                `Unsupported: invalidating more than a single resource set`
+            );
+        }
+        const paths = resourcesToInvalidate[0].include
+            .map((path) => `"/${path}"`)
+            .join(' ');
+        commands.push({
+            command: `aws cloudfront create-invalidation --distribution-id ${distribution} --paths ${paths}`
+        });
+    }
 }
